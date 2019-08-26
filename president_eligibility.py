37cbdf4df8bfba17fe9db9b8206712a184d13293
Dung (Justin) Le <dle21@ssis.edu.vn>
2019-08-26 11:01:49 +0700

    Update president_eligibility.py

--- president_eligibility.py
+++ president_eligibility.py
@@ -11,17 +11,17 @@ residency = int(input("Years of Residency: "))
 # If the user is at least 35 years old, is a U.S. citizen, and lived in the U.S.
 # for at least 14 years, they are eligible to run for president.
 if age >= 35 and citizen == "Yes" and residency >= 14:
-    print "You are eligible to run for president!"
+    print("You are eligible to run for president!")
 
 # Otherwise, they are not eligible to run for president.
 else:
-    print "You are not eligible to run for president."
+    print("You are not eligible to run for president.")
 
 # Tells the user what qualities they're missing to be eligible to run for
 # president.
 if age < 35:
-    print "You are too young. Youmust be at least 35 years old."
+    print("You are too young. Youmust be at least 35 years old.")
 if citizen != "Yes":
-    print "You must be born in the U.S. to run for president."
+    print("You must be born in the U.S. to run for president.")
 if residency < 14:
-    print "You have not been a resident for long enough."
+    print("You have not been a resident for long enough.")
